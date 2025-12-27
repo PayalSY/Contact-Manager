5eb3a94d90688f2bdcc15d5b787dae7500a39992
PayalSY <singhyadavpayal02@gmail.com>
2025-12-27 11:19:27 +0545

    Add files via upload

---
+++ src/components/Persons.js
@@ -0,0 +1,21 @@
+export const Dave = () => {
+  return (
+    <>
+      <div style={{ background: "green", color: "white" }}>I am dave </div>
+    </>
+  );
+};
+export const John = () => {
+  return (
+    <>
+      <div style={{ background: "green", color: "white" }}>This is John </div>
+    </>
+  );
+};
+export const Chris = () => {
+  return (
+    <>
+      <div style={{ background: "green", color: "white" }}>This is Chris </div>
+    </>
+  );
+};
